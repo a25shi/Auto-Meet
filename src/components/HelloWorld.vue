8551646bca873c4071bb8bfb7682d00a22602b53
Allen Shi <allen_shi2002@hotmail.com>
2025-08-24 19:25:58 -0400

    Added primevue config

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,16 +1,17 @@
+
+
 <script setup lang="ts">
 import { ref } from 'vue'
 
 defineProps<{ msg: string }>()
 
-const count = ref(0)
 </script>
 
 <template>
   <h1>{{ msg }}</h1>
 
   <div class="card">
-    <button type="button" @click="count++">count is {{ count }}</button>
+    <Button type="button" label="Search" icon="pi pi-search" />
     <p>
       Edit
       <code>components/HelloWorld.vue</code> to test HMR
